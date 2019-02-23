14af4816799436cd99661f79b8db658e47ed3d6f
victorjaquez <victor@jaquez.me>
2019-02-22 17:37:45 -0700

    display mock employee list

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Employee } from '../employee';
+import { EMPLOYEES } from '../mock-employees';
 
 @Component({
   selector: 'app-employees',
@@ -7,10 +7,7 @@ import { Employee } from '../employee';
   styleUrls: ['./employees.component.css']
 })
 export class EmployeesComponent implements OnInit {
-  employee: Employee = {
-    id: 1,
-    name: 'Victor'
-  };
+  employees = EMPLOYEES;
 
   constructor() { }
 
---
+++ src/app/mock-employees.ts
@@ -0,0 +1,14 @@
+import { Employee } from './employee'
+
+export const EMPLOYEES: Employee[] = [
+  {id: 1, name: 'Jazmin'},
+  {id: 2, name: 'Alvaro'},
+  {id: 3, name: 'Podrick'},
+  {id: 4, name: 'Judith'},
+  {id: 5, name: 'Natalia'},
+  {id: 6, name: 'Irma'},
+  {id: 7, name: 'Saul'},
+  {id: 8, name: 'Juan'},
+  {id: 9, name: 'Miguel'},
+  {id: 10, name: 'Paz'}
+]
