c8d5fc098e78f55ed84ec2573d83e3f2861328bd
victorjaquez <victor@jaquez.me>
2019-02-23 01:23:48 -0700

    dashboard routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeesComponent } from './employees/employees.component'
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 const routes: Routes = [
-  { path: 'employees', component: EmployeesComponent }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'employees', component: EmployeesComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
