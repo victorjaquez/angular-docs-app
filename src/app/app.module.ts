6819f78f060489c93aa8db5c4c1f928e930a76cb
victorjaquez <victor@jaquez.me>
2019-02-22 22:31:29 -0700

    MessageService, MessageComponent, styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
-    EmployeeDetailComponent
+    EmployeeDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { Employee }  from './employee';
 import { EMPLOYEES } from './mock-employees';
 import { Observable, of } from 'rxjs';
-
+import { MessageService } from './message.service' 
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
   getEmployees(): Observable<Employee[]> {
+    // TODO: send the message _after_ fetching the employees
+    this.messageService.add('EmployeeService: fetched employees')
     return of(EMPLOYEES);
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
