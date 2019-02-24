7a12ec4d4ea096cd378b467415af0b9b6d931671
victorjaquez <victor@jaquez.me>
2019-02-23 19:36:25 -0700

    Http error handling

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Employee }  from './employee';
-import { EMPLOYEES } from './mock-employees';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,11 +20,24 @@ export class EmployeeService {
     this.messageService.add(`EmployeeService: ${message}`)
   }
 
-
+  /** GET employees from the server */
   getEmployees(): Observable<Employee[]> {
     // TODO: send the message _after_ fetching the employees
     this.messageService.add('EmployeeService: fetched employees')
     return this.http.get<Employee[]>(this.employeesUrl)
+      .pipe(
+        tap(employees => this.log('fetched employees')),
+        catchError(this.handleError('getEmployees', []))
+      );
+  }
+
+  /** Handle Http operation that failed. */
+  private handleError<T> (operation = 'operation', result ?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error)
+      this.log(`${operation} failed: ${error.message}`)
+      return of(result as T)
+    }
   }
 
   getEmployee(id: number): Observable<Employee> {
